de6c483b8abc841dd52b0b9eb192dfd7abb2b75a
raster-gen <rasterroot@gmail.com>
2021-04-22 12:14:53 +0300

    add pictures

---
+++ app/src/main/java/com/example/tafficlight/Constance.kt
@@ -0,0 +1,5 @@
+package com.example.tafficlight
+
+object Constance {
+    const val colorBank = intArrayOf(R.drawable.ic_launcher_foreground)
+}
--- app/src/main/java/com/example/tafficlight/MainActivity.kt
+++ app/src/main/java/com/example/tafficlight/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val currentIndex = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
     }
 }
